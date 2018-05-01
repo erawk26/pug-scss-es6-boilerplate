caefddd927c8115b295e3283c88542a7aa6f454b
erik <erik@hark.bz>
2018-05-01 15:48:50 -0400

    skeleton commit

---
+++ app/scripts/scroll.js
@@ -0,0 +1,14 @@
+require('smoothscroll');
+const ele = document.getElementsByClassName('headshot').item(0);
+const btn = document.getElementById('back-to-top');
+
+function isScrolledIntoView (el) {
+	const rect = el.getBoundingClientRect();
+	const elemTop = rect.top;
+	const elemBottom = rect.bottom;
+	// const isCompletelyVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+	// const isPartiallyVisible = elemTop < window.innerHeight && elemBottom >= 0;
+	return (elemTop >= 0) && (elemBottom <= window.innerHeight);
+}
+
+window.onscroll = () => isScrolledIntoView(ele) ? btn.style.opacity = 0 : btn.style.opacity = 1;
